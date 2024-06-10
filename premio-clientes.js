d6be0bfaa995965d5f62bf4156fc623115ae683a
Carlos Mestre <carmesce@gmail.com>
2024-06-10 11:44:02 +0200

    Code Style

--- premio-clientes.js
+++ premio-clientes.js
@@ -1,21 +1,36 @@
 function acmeRegalo(clientes) {
-    var premioNavidad;
-    console.log("");
-    for (i = 0; i <= clientes.length - 1; i++) {
-        var cuantia = Math.floor(Math.random() * 15000);
-        if (cuantia > 9999) {
-            premioNavidad = "Cesta de navidad con jamón ";
-            console.log(clientes[i] + ": " + cuantia + " eur " + "(" + premioNavidad + ")");
-        } else if (cuantia >= 5000 && cuantia <= 9999) {
-            premioNavidad = "Cesta de navidad simplona ";
-            console.log(clientes[i] + ": " + cuantia + " eur " + "(" + premioNavidad + ")");
-        } else {
-            premioNavidad = "Carta y pegatina de Acme ";
-            console.log(clientes[i] + ": " + cuantia + " eur " + "(" + premioNavidad + ")");
-        }
+  var premioNavidad;
+  console.log("");
+  for (i = 0; i <= clientes.length - 1; i++) {
+    var cuantia = Math.floor(Math.random() * 15000);
+    if (cuantia > 9999) {
+      premioNavidad = "Cesta de navidad con jamón ";
+      console.log(
+        clientes[i] + ": " + cuantia + " eur " + "(" + premioNavidad + ")"
+      );
+    } else if (cuantia >= 5000 && cuantia <= 9999) {
+      premioNavidad = "Cesta de navidad simplona ";
+      console.log(
+        clientes[i] + ": " + cuantia + " eur " + "(" + premioNavidad + ")"
+      );
+    } else {
+      premioNavidad = "Carta y pegatina de Acme ";
+      console.log(
+        clientes[i] + ": " + cuantia + " eur " + "(" + premioNavidad + ")"
+      );
     }
-    console.log("");
+  }
+  console.log("");
 }
-acmeRegalo(["Juan Ramos", "Pedro Sanchez", "Begoña Gómez", "Frederick Williams",
-    "Roberta Williams", "Paco Porras", "La Veneno", "Profesor Moriarti",
-    "Pato Lucas", "Cristobal Montoro"]);
+acmeRegalo([
+  "Juan Ramos",
+  "Pedro Sanchez",
+  "Begoña Gómez",
+  "Frederick Williams",
+  "Roberta Williams",
+  "Paco Porras",
+  "La Veneno",
+  "Profesor Moriarti",
+  "Pato Lucas",
+  "Cristobal Montoro",
+]);
--- premio-clientes2.js
+++ premio-clientes2.js
@@ -1,34 +1,38 @@
 function acmeRegalo(clientes) {
-    var premioNavidad;
-    var retornoPantalla = [];
+  var premioNavidad;
+  var retornoPantalla = [];
 
-    for (i = 0; i <= clientes.length - 1; i++) {
+  for (i = 0; i <= clientes.length - 1; i++) {
+    var cuantia = Math.floor(Math.random() * 15000);
 
-        var cuantia = Math.floor(Math.random() * 15000);
-
-        if (cuantia > 9999) {
-            premioNavidad = "Cesta de navidad con jamón ";
-           
-        } else if (cuantia >= 5000 && cuantia <= 9999) {
-            premioNavidad = "Cesta de navidad simplona ";
-           
-        } else {
-            premioNavidad = "Carta y pegatina de Acme ";
-            
-        }
-        retornoPantalla[i] = [clientes[i], cuantia, premioNavidad];
-
+    if (cuantia > 9999) {
+      premioNavidad = "Cesta de navidad con jamón ";
+    } else if (cuantia >= 5000 && cuantia <= 9999) {
+      premioNavidad = "Cesta de navidad simplona ";
+    } else {
+      premioNavidad = "Carta y pegatina de Acme ";
     }
-    return retornoPantalla;
+    retornoPantalla[i] = [clientes[i], cuantia, premioNavidad];
+  }
+  return retornoPantalla;
 }
 
-var retorno = acmeRegalo(["Juan Ramos", "Pedro Sanchez", "Begoña Gómez", "Frederick Williams",
-    "Roberta Williams", "Paco Porras", "La Veneno", "Profesor Moriarti",
-    "Pato Lucas", "Cristobal Montoro"]);
+var retorno = acmeRegalo([
+  "Juan Ramos",
+  "Pedro Sanchez",
+  "Begoña Gómez",
+  "Frederick Williams",
+  "Roberta Williams",
+  "Paco Porras",
+  "La Veneno",
+  "Profesor Moriarti",
+  "Pato Lucas",
+  "Cristobal Montoro",
+]);
 
-let index = 0
-retorno.forEach(cliente => {
-    cliente.forEach(item => { 
-        document.getElementById([index++]).innerHTML = item;
-    })
+let index = 0;
+retorno.forEach((cliente) => {
+  cliente.forEach((item) => {
+    document.getElementById([index++]).innerHTML = item;
+  });
 });
