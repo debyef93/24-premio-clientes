176afd6ba05164d88af59db2a0a46e0917413760
Hermesgb <hermesgabriel@gmail.com>
2024-06-10 09:17:08 +0200

    Reducidas lineas de código en jss.

--- premio-clientes2.js
+++ premio-clientes2.js
@@ -22,38 +22,13 @@ function acmeRegalo(clientes) {
     return retornoPantalla;
 }
 
-retorno = acmeRegalo(["Juan Ramos", "Pedro Sanchez", "Begoña Gómez", "Frederick Williams",
+var retorno = acmeRegalo(["Juan Ramos", "Pedro Sanchez", "Begoña Gómez", "Frederick Williams",
     "Roberta Williams", "Paco Porras", "La Veneno", "Profesor Moriarti",
     "Pato Lucas", "Cristobal Montoro"]);
 
-document.getElementById([0]).innerHTML = retorno[0][0];
-document.getElementById([1]).innerHTML = retorno[0][1];
-document.getElementById([2]).innerHTML = retorno[0][2];
-document.getElementById([3]).innerHTML = retorno[1][0];
-document.getElementById([4]).innerHTML = retorno[1][1];
-document.getElementById([5]).innerHTML = retorno[1][2];
-document.getElementById([6]).innerHTML = retorno[2][0];
-document.getElementById([7]).innerHTML = retorno[2][1];
-document.getElementById([8]).innerHTML = retorno[2][2];
-document.getElementById([9]).innerHTML = retorno[3][0];
-document.getElementById([10]).innerHTML = retorno[3][1];
-document.getElementById([11]).innerHTML = retorno[3][2];
-document.getElementById([12]).innerHTML = retorno[4][0];
-document.getElementById([13]).innerHTML = retorno[4][1];
-document.getElementById([14]).innerHTML = retorno[4][2];
-document.getElementById([15]).innerHTML = retorno[5][0];
-document.getElementById([16]).innerHTML = retorno[5][1];
-document.getElementById([17]).innerHTML = retorno[5][2];
-document.getElementById([18]).innerHTML = retorno[6][0];
-document.getElementById([19]).innerHTML = retorno[6][1];
-document.getElementById([20]).innerHTML = retorno[6][2];
-document.getElementById([21]).innerHTML = retorno[7][0];
-document.getElementById([22]).innerHTML = retorno[7][1];
-document.getElementById([23]).innerHTML = retorno[7][2];
-document.getElementById([24]).innerHTML = retorno[8][0];
-document.getElementById([25]).innerHTML = retorno[8][1];
-document.getElementById([26]).innerHTML = retorno[8][2];
-document.getElementById([27]).innerHTML = retorno[9][0];
-document.getElementById([28]).innerHTML = retorno[9][1];
-document.getElementById([29]).innerHTML = retorno[9][2];
-
+let index = 0
+retorno.forEach(cliente => {
+    cliente.forEach(item => { 
+        document.getElementById([index++]).innerHTML = item;
+    })
+});
